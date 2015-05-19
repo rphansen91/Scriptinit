eea82598a8f7b0cb3ed69ce730ea784f3b2896c9
rphansen91 <rphansen91@gmail.com>
2015-05-19 03:13:15 -0700

    Bootstrap and users

--- client/lib/app.js
+++ client/lib/app.js
@@ -1,11 +1,49 @@
-angular.module('scriptInit',['angular-meteor']);
+angular.module('scriptInit', ['angular-meteor'])
 
-angular.module('scriptInit').directive('mainView', ['$meteor', function ($meteor) {
+.factory('subscribeUser', ['$meteor', function ($meteor) {
+  var factory = {}
+
+  factory.getUsers = function () {
+    return $meteor.collection(Users)
+  }
+
+  factory.addUser = function (usersCollection, user) {
+      if (user && user.name.length && user.email.indexOf('@') != -1) {
+        usersCollection.push(user)
+      }
+  }
+
+  return factory
+}])
+
+.directive('subscribe', ['subscribeUser', function (subscribeUser) {
   return {
     restrict: 'EA',
-    templateUrl: 'client/core/mainView.ng.html',
+    templateUrl: 'client/core/subscribe.ng.html',
     link: function (scope, element, attrs) {
-      scope.parties = $meteor.collection(Parties)
+      scope.users = subscribeUser.getUsers()
+
+      scope.newUser = {}
+      scope.newUser.name = ""
+      scope.newUser.email = ""
+
+      scope.subscribe = function () {
+        console.log(scope.users, scope.newUser);
+        subscribeUser.addUser(scope.users, scope.newUser)
+      }
+
+    }
+  }
+}])
+
+.directive('viewBlog', ['$window', function ($window) {
+  return {
+    restrict: 'EA',
+    templateUrl: 'client/core/viewBlog.ng.html',
+    link: function (scope, element, attrs) {
+      scope.viewBlog = function () {
+        $window.open('http://blog.scriptinit.com')
+      }
     }
   }
 }])
